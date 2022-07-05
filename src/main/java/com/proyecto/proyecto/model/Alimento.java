75b4f7d226d4e3f9c8d1b22c0838a78aec05e767
kellysolanomt <kelly.solano.1403@gmail.com>
2022-07-05 08:23:02 -0500

    Correcion del footer

--- src/main/java/com/proyecto/proyecto/model/Alimento.java
+++ src/main/java/com/proyecto/proyecto/model/Alimento.java
@@ -4,11 +4,9 @@
  */
 package com.proyecto.proyecto.model;
 
-import java.io.Serializable;
 import java.util.Date;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.PrimaryKeyJoinColumn;
 import javax.persistence.Table;
 
 /**
@@ -16,31 +14,32 @@ import javax.persistence.Table;
  * @author kelly
  */
 @Entity
-@Table(name="pro_alimentos")
-public class Alimento extends Producto{
-    
-    @Column(name="ali_fecha_venc")
-    private Date fechaVencimiento;
-    @Column(name="ali_consumo")
+@Table(name = "pro_alimentos")
+public class Alimento extends Producto {
+
+   
+    @Column(name = "ali_fecha_venc")
+    private String fechaVencimiento;
+    @Column(name = "ali_consumo")
     private boolean listoParaConsumir;
-    @Column(name="ali_origen_ani")
+    @Column(name = "ali_origen_ani")
     private boolean origenAnimal;
-    
+
     public Alimento() {
         super();
     }
-   
-    public Alimento(Date fechaVencimiento, boolean listoParaConsumir, boolean origenAnimal) {
+
+    public Alimento(String fechaVencimiento, boolean listoParaConsumir, boolean origenAnimal) {
         this.fechaVencimiento = fechaVencimiento;
         this.listoParaConsumir = listoParaConsumir;
         this.origenAnimal = origenAnimal;
     }
-    
-    public Date getFechaVencimiento() {
+
+    public String getFechaVencimiento() {
         return fechaVencimiento;
     }
 
-    public void setFechaVencimiento(Date fechaVencimiento) {
+    public void setFechaVencimiento(String fechaVencimiento) {
         this.fechaVencimiento = fechaVencimiento;
     }
 
--- src/main/resources/static/js/js.js
+++ src/main/resources/static/js/js.js
@@ -44,13 +44,7 @@ window.onload = function(){
     recuperarUsuario();
 };
 
-$("input").on("change", function() {
-    this.setAttribute(
-        "data-date",
-        moment(this.value, "DD-MM-YYYY")
-        .format( this.getAttribute("data-date-format") )
-    )
-}).trigger("change")
+
 
 
 
