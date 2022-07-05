a1f207a07df0da55129ff9cc81d6764c8c989aec
kellysolanomt <kelly.solano.1403@gmail.com>
2022-07-05 00:31:46 -0500

    Correccion

--- src/main/resources/static/js/js.js
+++ src/main/resources/static/js/js.js
@@ -44,5 +44,13 @@ window.onload = function(){
     recuperarUsuario();
 };
 
+$("input").on("change", function() {
+    this.setAttribute(
+        "data-date",
+        moment(this.value, "YYYY-MM-DD")
+        .format( this.getAttribute("data-date-format") )
+    )
+}).trigger("change")
+
 
 
